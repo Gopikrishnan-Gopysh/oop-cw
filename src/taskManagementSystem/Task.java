8af325adc7ee340aae92845be7818aa2957a595c
gopikrishnang <gopikrishnang@gkg-macbook-pro.du.hw.ac.uk>
2024-01-24 10:00:16 +0400

    commit 1.25

--- src/taskManagementSystem/Task.java
+++ src/taskManagementSystem/Task.java
@@ -1,5 +1,21 @@
 package taskManagementSystem;
 
-public class Task {
-
+public class Task {    //Fields
+	private int ID;
+	private String Name;
+	private String Description;
+	
+	
+	public Task(int ID, String Name, String Description) {
+		this.ID = ID;
+		this.Name = Name;
+		this.Description = Description;
+		
+	}
+	public Task(int ID, String Name) {
+		this.ID = ID;
+		this.Name = Name;
+		}
+	return String getID('');
+	
 }
